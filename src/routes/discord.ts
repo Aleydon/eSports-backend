b37cac0d33078c1b73f7fc03795884fda858cba8
Aleydon <aleydonroberto@gmail.com>
2025-02-24 12:40:57 -0300

    UP: Create discord list

--- src/routes/discord.ts
+++ src/routes/discord.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from 'express';
-
 import { router } from '@constants/router';
+
+import { prisma } from '@constants/prisma';
 
 export const getDiscord = router.get(
 	'/ads/:adsID/discord',
-	(req: Request, res: Response) => {
-		return res.status(200).json([]);
+	async (req: Request, res: Response) => {
+		const { adsID } = req.params;
+		const ad = await prisma.ad.findUniqueOrThrow({
+			select: {
+				discord: true
+			},
+			where: { id: adsID }
+		});
+		return res.status(200).json({
+			discord: ad.discord
+		});
 	}
 );
